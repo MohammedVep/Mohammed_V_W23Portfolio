8d09381cc0f8caddc9e20939887694b916637f63
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-19 21:29:55 -0500

    Added Resume

--- Mohammed_V_W23Portfolio/client/src/components/header/CTA.jsx
+++ Mohammed_V_W23Portfolio/client/src/components/header/CTA.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import CV from '../../assets/MohammedVepari(CV).pdf'
+import Resume from '../../assets/MohammedVepari.pdf'
 
 const CTA = () => {
   return (
     <div className='CTA'>
         <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn=primary'>Let's Talk</a>
+        <a href={Resume} download className='btn'>Download Resume</a>
     </div>
   )
 }
